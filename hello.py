9289eaa384064578eb984ed61994561ce62ec44c
Kales <kales5ails@gmail.com>
2026-03-04 18:12:57 +1100

    Update hello.py
    Input notes to break down the function

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
 print("Hello World")
+
+## Notes:
+  ## Print() = Sends a specificed message to an output (E.g. Control/Terminal Screen)
+  ## Paranentheses --> () = Required to enclose define.
+  ## "" = String 
